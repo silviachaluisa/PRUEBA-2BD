3081a544fe5b27f46493ec281c9d71d30f2271ec
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-02 11:29:36 -0500

    Ventana 2 Marcelo

---
+++ Ventana2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Ventana2 {
+    private JLabel titulo;
+    private JRadioButton verSaldoRadioButton;
+    private JRadioButton retiroRadioButton;
+    private JRadioButton depositoRadioButton;
+    private JRadioButton salirRadioButton;
+}
