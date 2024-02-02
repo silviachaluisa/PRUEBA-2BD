7ee524b4fdef53d3114dc55ee2607a0882ded2fa
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-02 11:29:38 -0500

    Ventana 1

--- Ventana1.java
+++ Ventana1.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
 
 public class Ventana1 {
-    private JPanel Bienvenido;
-    private JTextField textField1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
+    JPanel Bienvenido;
+    private JButton Boton1;
+    private JButton Boton3;
+    private JButton Boton7;
+    private JButton Boton8;
+    private JButton Boton2;
+    private JButton Boton4;
+    private JButton Boton5;
+    private JButton Boton6;
+    private JButton Boton9;
+    private JButton Boton10;
+    private JButton Boton11;
+    private JPasswordField PSW1;
 }
