cd4ec2a0413e14dab6a9596ef872e5c5571bc7c0
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-02 12:03:48 -0500

    Ventana 6

---
+++ Ventana6.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Ventana6 {
+    private JPanel Ventana6;
+    private JTextField textField1;
+    private JButton a1Button;
+    private JButton a4Button;
+    private JButton a7Button;
+    private JButton a0Button;
+    private JButton a3Button;
+    private JButton a6Button;
+    private JButton a9Button;
+    private JButton enterButton;
+    private JButton a2Button;
+    private JButton a5Button;
+    private JButton a8Button;
+}
