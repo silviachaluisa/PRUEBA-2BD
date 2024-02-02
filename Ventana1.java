34ebd7757f1306d1634c37f8602162474bb15a47
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-02 11:59:22 -0500

    Mas ventana Marcelo

--- Ventana1.java
+++ Ventana1.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
 
 public class Ventana1 {
     JPanel Bienvenido;
@@ -14,4 +17,106 @@ public class Ventana1 {
     private JButton Boton10;
     private JButton Boton11;
     private JPasswordField PSW1;
+
+    public Ventana1() {
+        Boton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"1"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"2"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"3"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"4"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"5"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"6"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"7"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"8"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"9"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ingreso = new String(PSW1.getPassword()); // Obtener la contraseña y convertirla a String
+                PSW1.setText(ingreso+"0"); // Setear el texto del JPasswordField como ingreso y el numero del boton seleccionada
+                System.out.println("Ingreso: "+ ingreso);
+            }
+        });
+        Boton11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String credencial = new String (PSW1.getPassword());
+                if (credencial.equals("202121334"))
+                {
+                    JOptionPane.showMessageDialog(Bienvenido, "Acceso correcto", "Credenciales Validas!", JOptionPane.INFORMATION_MESSAGE);
+                    JFrame ventana=new JFrame("Transacciones!");
+                    ventana.setSize(300, 300);
+                    ventana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                    ventana.setContentPane(new Ventana2().Ventana2);
+                    ventana.setVisible(true);
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(Bienvenido, "Acceso denegado!", "Credenciales invalidas", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
 }
--- Ventana2.java
+++ Ventana2.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana2 {
     private JLabel titulo;
@@ -7,4 +9,18 @@ public class Ventana2 {
     private JRadioButton depositoRadioButton;
     private JRadioButton salirRadioButton;
     JPanel Ventana2;
+
+
+    public Ventana2() {
+        verSaldoRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame saldo = new JFrame("Saldo de tu cuenta");
+                saldo.setSize(300, 300);
+                saldo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                saldo.setContentPane(new Ventana4().Ventan4);
+                saldo.setVisible(true);
+            }
+        });
+    }
 }
