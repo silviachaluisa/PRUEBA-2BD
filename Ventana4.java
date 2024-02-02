dc5a2490a2a4b468c14498699b718b2be49c5008
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-02 12:15:28 -0500

    Mas de 4 ventana Marcelo

--- Ventana4.java
+++ Ventana4.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana4 {
     JPanel Ventan4;
     private JButton BotonMenu1;
+
+    public Ventana4() {
+        BotonMenu1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- Ventana5.java
+++ Ventana5.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana5 {
     private JTextField TXF1;
@@ -10,7 +12,90 @@ public class Ventana5 {
     private JButton a7Button;
     private JButton a2Button;
     private JButton a5Button;
-    private JButton a10Button;
+    private JButton a0Button;
     private JButton a8Button;
     private JButton enterButton;
+    JPanel Ventana5;
+
+    public Ventana5() {
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"1"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"2"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"3"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"4"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"5"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"6"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"7"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"8"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"9"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Monto = TXF1.getText(); // Obtener la cantidad ingresada
+                TXF1.setText(Monto+"0"); // Setear el texto del JTextField como monto y el numero del boton seleccionado
+            }
+        });
+    }
 }
